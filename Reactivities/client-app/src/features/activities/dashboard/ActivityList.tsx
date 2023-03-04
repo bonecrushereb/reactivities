b8c3eab540af002fba6bd8a8ebeef402dbe437e6
bonecrushereb <bonecrushereb@gmail.com>
2023-03-04 16:36:16 -0500

    added form

--- Reactivities/client-app/src/features/activities/dashboard/ActivityList.tsx
+++ Reactivities/client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Item, Segment } from 'semantic-ui-react';
+import { Button, Item, Label, Segment } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 
 interface Props {
@@ -21,6 +21,7 @@ export default function ActivityList({activities}: Props) {
               </Item.Description>
               <Item.Extra>
                 <Button floated='right' content='View' color='blue'/>
+                <Label basic content={activity.category} />
               </Item.Extra>
             </Item.Content>
           </Item>
