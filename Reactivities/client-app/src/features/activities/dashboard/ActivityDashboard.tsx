58afa7b95fcfcfddc3d374ccd14ef0258aa2d233
bonecrushereb <bonecrushereb@gmail.com>
2023-03-15 10:16:51 -0400

    persistent create and update

--- Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,9 +15,10 @@ interface Props {
   closeForm: () => void;
   createOrEdit: (activity: Activity) => void;
   deleteActivity: (id: string) => void;
+  submitting: boolean;
 }
 
-export function ActivityDashboard({activities, selectActivity, deleteActivity,
+export function ActivityDashboard({activities, selectActivity, deleteActivity, submitting,
     selectedActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit}: Props) {
   return (
     <Grid>
@@ -36,7 +37,12 @@ export function ActivityDashboard({activities, selectActivity, deleteActivity,
           openForm={openForm}
         />}
         {editMode &&
-        <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
+        <ActivityForm 
+          closeForm={closeForm} 
+          activity={selectedActivity} 
+          createOrEdit={createOrEdit}
+          submitting={submitting}
+        />}
       </Grid.Column>
     </Grid>
   );
